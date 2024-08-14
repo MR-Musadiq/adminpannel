d5a794b0a0d53915895c32d9caf232f85f6aef26
MR-Musadiq <161106716+MR-Musadiq@users.noreply.github.com>
2024-08-14 06:11:29 -0700

    Update

--- add_product.php
+++ add_product.php
@@ -1,8 +1,10 @@
 <?php
-include('sidebar.php')
-include('');
+include('sidebar.php');
+include('connection.php')
 ?>
+
 <div class="container">
+  <h1>Add product</h1>
              <form action="" method="post"  enctype="multipart/form-data">
         <div class="for-group">
         <label for="">P:name</label>
@@ -25,7 +27,7 @@ include('');
          <select name="cat" id="">
           <option value=""> select any category</option>
             <?php
-       $data=mysqli_query($con,"select * from category ");
+       $data=mysqli_query($con,"SELECT * FROM `category`");
        while($cat=mysqli_fetch_array($data)){?>
          <option value="<?php echo $cat[0]?>"><?php echo $cat[1]?></option>
          <?php
@@ -36,7 +38,8 @@ include('');
          </select>
 
         </div>
-        <input type="submit" value="product" name="add_pro" class="btn btn-info">
+        <input type="submit" value="Add_product" name="add_pro" class="btn btn-info">
+        <a href="view_product.php"><button class="btn btn-primary">view</button></a>
 
         
 
@@ -48,19 +51,20 @@ include('');
              <?php
  if(isset($_POST['add_pro'])){
     $name=$_POST['pname'];
-    $des=$_POST['price'];
+    $price=$_POST['price'];
     $qua=$_POST['quaninty'];
     $cat=$_POST['cat'];
     $pro_image=$_FILES['pimage']['name'];
     $tamp_imagename=$_FILES['pimage']['tmp_name'];
-    $desination="img/".$image;
-    $extension=pathinfo($image , PATHINFO_EXTENSION);
+    $desination="img/". $pro_image;
+    $extension=pathinfo( $pro_image , PATHINFO_EXTENSION);
     if($extension=='png' || $extension=='jpeg' || $extension=='jpg'){
         if(move_uploaded_file($tamp_imagename, $desination)){
-          $query= mysqli_query($con,"INSERT INTO `product`( `name`, `price`, `quaninty`, `image`, `category`) VALUES ('$name',' $des',' $qua',' '$pro_image',  $cat',')");
+          $query= mysqli_query($con,"INSERT INTO `product`( `name`, `price`, `quantity`, `image`, `category_id`) VALUES ('  $name',' $price','  $qua','   $pro_image',' $cat')");
             if($query){
                 
-                    echo"<script>alert('product addded ')</script>";
+                    echo"<script>alert('product addded ')
+                    location.assign('view_product.php')</script>";
             
             }
            
@@ -93,7 +97,22 @@ include('');
 </html>
 
   <!-- Footer -->
-  <?php
-            include('footer.php')
-            ?>
+ 
             <!-- End of Footer -->
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
+<!-- Footer -->
+<?php
+           include('footer.php');
+           ?>
