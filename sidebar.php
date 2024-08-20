22772dff8776493612a51e495887aded07d8b8a8
MR-Musadiq <161106716+MR-Musadiq@users.noreply.github.com>
2024-08-20 10:51:10 +0500

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,8 @@
+
+<?php
+session_start()
+;?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -329,7 +334,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $_SESSION['username']?></span>
                                 <img class="img-profile rounded-circle"
                                     src="img/undraw_profile.svg">
                             </a>
@@ -349,7 +354,7 @@
                                     Activity Log
                                 </a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                <a class="dropdown-item" href="login.php" >
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
                                 </a>
